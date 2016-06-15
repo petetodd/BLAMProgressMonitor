a69ff877f6d0af9df763ce1f854a626c39ed5d1f
Peter Todd <peter@petertodd.com>
2016-06-15 18:09:55 +0100

    Initial Commit

---
+++ BLAMProgressMonitor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BLAMProgressMonitor
+//
+//  Created by Peter Todd Air on 15/06/2016.
+//  Copyright © 2016 Bright Green Star. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
